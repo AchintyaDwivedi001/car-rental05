fa5e0b8f90009bf4524d7a0d4f769d1775f54af5
AchintyaDwivedi001 <achintyadwivedi735@gmail.com>
2025-09-28 19:38:54 +0530

    yes

--- src/components/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",  // Enable static HTML export
-  images: {
-    unoptimized: true // required if using Next.js Image component
-  }
-};
-
-module.exports = nextConfig;
